9fd767985c01e84e646371eb1263323a58aa890b
Jose Zavala <jmzg1229@gmail.com>
2019-07-23 21:28:20 -0400

    Initial Python Math Interpreter creation
    - Added files to visual studio
    - imported mathml and did some minor edits
    - More dependency comments on math_interpreter.py

--- math_interpreter.py
+++ math_interpreter.py
@@ -2,8 +2,13 @@
 # Mathematical Interpretation Tool (MIT, lol)
 
 # Required packages:
+# For sympy & latex:
 # sympy
 # antlr-python-runtime (for Latex parsing)
+#
+# For mathml experimental module:
+# lxml
+# StringIO
 
 # sympy import
 from sympy.parsing.latex import parse_latex
@@ -12,7 +17,10 @@ from sympy.parsing.latex import parse_latex
 
 # Convert latex to sympy expression using parse_latex(s)
 # s is a RAW string of latex expression
-s = r"\frac {1 + \sqrt {\a}} {\b}"
+s0 = r"\frac {1 + \sqrt {\a}} {\b}"
+s1 = r"\frac {P_{0}} {P}"
+
+s = s1
 expr = parse_latex(s)
 print("Latex:", s)
 print("Sympy expression:", expr)
